3cc41746773448914e6fc4912dc142b27da0245c
Dmitry Arzhevitin <adachel74@gmail.com>
2024-02-23 14:43:48 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Probe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*byte b = 20;
+            int i = 100;
+            i = b;
+            Console.WriteLine(i);*/
+
+
+            byte a = 20;
+            int c = 256;
+            a = (byte)c;
+
+            Console.WriteLine(a); 
+
+        }
+    }
+}
